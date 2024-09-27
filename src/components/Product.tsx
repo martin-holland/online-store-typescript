891bd0469b09a4e6e4cc7413e0c0185bfa6b11fe
Martin Holland <martin.holland@free.fi>
2024-09-27 11:01:00 +0300

    mid-lesson lunch

---
+++ src/components/Product.tsx
@@ -0,0 +1,30 @@
+interface IProps {
+  image: string;
+  title: string;
+  price: number;
+  description: string;
+}
+
+export default function Product(props: IProps) {
+  const { image, title, price, rating, description } = props;
+
+  return (
+    <div>
+      <section className="Detail">
+        <article className="Detail_thumbnail">
+          <img style={{ maxWidth: "10%" }} src={image} />
+        </article>
+        <article className="Detail_content">
+          <h2>{title}</h2>
+        </article>
+        <article className="Detail_info">
+          <span className="Detail_price">${price}</span>
+          <span className="Detail_rate">{rating.rate}</span>
+        </article>
+        <article className="Detail_description">
+          <p>{description}</p>
+        </article>
+      </section>
+    </div>
+  );
+}
